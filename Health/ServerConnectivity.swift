5b4c1a23f0824c7c9d8b3ca3ce9a6d9163075581
Anil <anil.prasad@hindustanwellness.com>
2017-02-16 17:29:28 +0530

    - Update in FamilyMemberInfo - Add Alamofire in Server Connectivity

--- Health/ServerConnectivity.swift
+++ Health/ServerConnectivity.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ServerConnectivity: NSObject, XMLParserDelegate {
     var mutableData:NSMutableData  = NSMutableData()
@@ -25,7 +26,7 @@ class ServerConnectivity: NSObject, XMLParserDelegate {
   
     
     
-    internal func callWebservice(_ dictParameters : Dictionary<String, String> , resulttagname : String , methodname : String , className : serverTaskComplete)  {
+ /*   internal func callWebservice(_ dictParameters : Dictionary<String, String> , resulttagname : String , methodname : String , className : serverTaskComplete)  {
         resultTagName = resulttagname
         myClass = className
         methodName = methodname
@@ -79,159 +80,144 @@ class ServerConnectivity: NSObject, XMLParserDelegate {
 //            
 //        }
     
-    }
+    } */
     
     
- /*   internal func callWebserviceForImageUploading(dictParameters : Dictionary<String, String> , resulttagname : String , methodname : String ,className : serverTaskComplete)  {
-        
-        
-    /*    resultTagName = resulttagname
-        myClass = className
-        methodName = methodname
-       
-        
-       
-        
-        var soapURL = String(format:"<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><%@ xmlns='http://23.101.24.132/healthservice_IOS/webservice.asmx'>",methodName)
-        // var soapURL = String(format:"<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><%@ xmlns='http://23.101.24.132/healthservice_IOS/webservice.asmx'>",methodName)
-        
-        for i in (0..<dictParameters.count) {
-            let key = [String](dictParameters.keys)[i]
-            let value = [String](dictParameters.values)[i]
-            let newParams = String(format: "<%@>%@</%@>",key,value,key)
-            let newUrl = soapURL + newParams
-            soapURL = newUrl
-        }
-        let endUrl = String(format:"<eType>I</eType></%@></soap12:Body></soap12:Envelope>",methodName)
-        let finalSoapUrl = soapURL + endUrl
-        
-       // let urlString = "http://23.101.24.132/healthservice/webservice.asmx"
-        let urlString = "http://23.101.24.132/healthservice_IOS/webservice.asmx"
-        let url = NSURL(string: urlString)
-        let theRequest = NSMutableURLRequest(URL: url!)
-        let msgLength = finalSoapUrl.characters.count
-        
-        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        theRequest.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
-        theRequest.HTTPMethod = "POST"
-        theRequest.timeoutInterval = 30
-        theRequest.HTTPBody = finalSoapUrl.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) // or false
-        
-        let boundary = "---------------------------14737809831466499882746641449"
-        let contentType = String(format: "multipart/form-data; boundary=%@",boundary)
-        theRequest.addValue(contentType, forHTTPHeaderField: "Content-Type")
-        let postbody = NSMutableData()
-        postbody.appendData("--\(boundary)\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        postbody.appendData("Content-Disposition:form-data; name=\"test\";filename=\"anil.pdf\"\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        postbody.appendData("Content-Type:image/jpg\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        postbody.appendData(pdfData)
-        postbody.appendData("\r\n--(boundary)--\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        theRequest.HTTPBody = postbody
-        
-        
-        let session = NSURLSession.sharedSession()
-        
-        
-        let task = session.dataTaskWithRequest(theRequest) {
-            (
-            let data, let response, let error) in
-            
-            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
-     
-                return
-            }
-            
-            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-     
-            
-        }
-        
-        task.resume()
-        
-        
-        //        let connection = NSURLConnection(request: theRequest, delegate: self, startImmediately: true)
-        //        connection!.start()
-        //
-        //        if (connection == true) {
-        //            let mutableData : Void = NSMutableData.initialize()
-        //
-        //        }  */
-        
-        
-        
-        
-        
-        
+    
+    
+    func callWebservice(_ dictParameters : Dictionary<String, String> , resulttagname : String , methodname : String , className : serverTaskComplete){
         resultTagName = resulttagname
         myClass = className
         methodName = methodname
-        
-        
-        //  var soapURL = String(format:"<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><%@ xmlns='http://23.101.24.132/healthservice/webservice.asmx'>",methodName)
-        var soapURL = String(format:"<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><%@ xmlns='http://23.101.24.132/healthservice_IOS/webservice.asmx'>",methodName)
+        var soapURL = String(format:"<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><%@ xmlns='http://23.101.24.132/healthservice/webservice.asmx'>",methodname)
         
         for i in (0..<dictParameters.count) {
             let key = [String](dictParameters.keys)[i]
-            let value = [String](dictParameters.values)[i]
+            var value : String!
+            
+            if methodname == "SaveRecord_New" && key == "recordString"{
+                value = [String](dictParameters.values)[i]
+            }else{
+                // let value = [String](dictParameters.values)[i]
+                value = AESEncryptionDecryption().EncryptAstring([String](dictParameters.values)[i])
+            }
             let newParams = String(format: "<%@>%@</%@>",key,value,key)
             let newUrl = soapURL + newParams
             soapURL = newUrl
         }
-        let endUrl = String(format:"<eType>I</eType></%@></soap12:Body></soap12:Envelope>",methodName)
-        let finalSoapUrl = soapURL + endUrl
         
-        //    let urlString = "http://23.101.24.132/healthservice/webservice.asmx"
-        let urlString = "http://23.101.24.132/healthservice_IOS/webservice.asmx"
-        let url = NSURL(string: urlString)
-        let theRequest = NSMutableURLRequest(URL: url!)
+        let endUrl = String(format:"<eType>I</eType></%@></soap12:Body></soap12:Envelope>",methodname)
+        let finalSoapUrl = soapURL + endUrl
         let msgLength = finalSoapUrl.characters.count
         
-        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        theRequest.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
-        theRequest.HTTPMethod = "POST"
-        theRequest.timeoutInterval = 60
-        theRequest.HTTPBody = finalSoapUrl.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) // or false
-        let connection = NSURLConnection(request: theRequest, delegate: self, startImmediately: true)
-        connection!.start()
-        
-        if (connection == true) {
+        do{
+            var request = try URLRequest(url: URL(string: "http://23.101.24.132/healthservice/webservice.asmx")!, method: .post)
+            request.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
+            request.httpBody = finalSoapUrl.data(using: String.Encoding.utf8, allowLossyConversion: false) // or false
+            let _ = Alamofire.request(request)
+                .responseData{ response in
+                    /*        do{
+                     
+                     
+                     //  let responseDict = try XMLReader.dictionary(forXMLString: result)
+                     let responseDict = try XMLReader.dictionary(forXMLData: response.data!)
+                     print(responseDict)
+                     
+                     var result = ""
+                     let string = String(describing: responseDict)
+                     if (self.methodName == "GetRecordsFromServer"){
+                     
+                     
+                     result = string as AnyObject as! String
+                     }else{
+                     
+                     result = AESEncryptionDecryption().DecryptAstring(string)
+                     }
+                     
+                     print(result)
+                     
+                     
+                     
+                     var newString = ""
+                     if self.methodName == "GetTestDetails" || self.methodName == "GetOrderInformation" {
+                     newString = result.replacingOccurrences(of: "\r\n", with: "", options: NSString.CompareOptions.literal, range: nil)
+                     }else{
+                     newString = result.replacingOccurrences(of: "\n", with: "", options: NSString.CompareOptions.literal, range: nil)
+                     }
+                     
+                     
+                     
+                     if (result .range(of: "[") != nil) {
+                     print(result)
+                     let json: AnyObject? = newString.parseJSONString
+                     
+                     print(json ?? "")
+                     self.myClass!.getAllResponse(json!, methodName: self.methodName)
+                     }else{
+                     print("Simple stingn")
+                     self.myClass!.getAllResponse(result as AnyObject, methodName: self.methodName)
+                     }
+                     
+                     }catch{
+                     print("Enable to Parse Data")
+                     } */
+                    
+                    let xmlParser = XMLParser(data: response.data!)
+                    xmlParser.delegate = self
+                    xmlParser.parse()
+                    xmlParser.shouldResolveExternalEntities = true
+            }
+            //                .responseJSON { response in
+            //
+            //
+            //
+            //
+            //                    print(response.request)  // original URL request
+            //                    print(response.response) // HTTP URL response
+            //                    print(response.data)     // server data
+            //                    print(response.result)   // result of response serialization
+            //
+            //                    if let JSON = response.result.value {
+            //                        print("JSON: \(JSON)")
+            //                    }
+            //            }
             
             
-            let mutableData : Void = NSMutableData.initialize()
+        }catch{
             
         }
-    } */
+    }
 
-    
 
     // MARK: - NSURLConnectionDeledate
    
     
-    func connection(_ connection: NSURLConnection!, didReceiveResponse response: URLResponse!) {
-        mutableData.length = 0;
-    }
-    
-    func connection(_ connection: NSURLConnection!, didReceiveData data: Data!) {
-        mutableData.append(data)
-    }
-    func connection(_ conn:NSURLConnection!, didFailWithError error:NSError!) {
-        
-        print(error)
-        
-        myClass!.getAllResponse("error" as AnyObject, methodName: methodName)
-        print("fail with error")
-       
-    }
-    
-    func connectionDidFinishLoading(_ connection: NSURLConnection!) {
-        let response = NSString(data: mutableData as Data, encoding: String.Encoding.utf8.rawValue)
-        
-        
-        let xmlParser = XMLParser(data: mutableData as Data)
-        xmlParser.delegate = self
-        xmlParser.parse()
-        xmlParser.shouldResolveExternalEntities = true
-    }
+//    func connection(_ connection: NSURLConnection!, didReceiveResponse response: URLResponse!) {
+//        mutableData.length = 0;
+//    }
+//    
+//    func connection(_ connection: NSURLConnection!, didReceiveData data: Data!) {
+//        mutableData.append(data)
+//    }
+//    func connection(_ conn:NSURLConnection!, didFailWithError error:NSError!) {
+//        
+//        print(error)
+//        
+//        myClass!.getAllResponse("error" as AnyObject, methodName: methodName)
+//        print("fail with error")
+//       
+//    }
+//    
+//    func connectionDidFinishLoading(_ connection: NSURLConnection!) {
+//        let response = NSString(data: mutableData as Data, encoding: String.Encoding.utf8.rawValue)
+//        
+//        
+//        let xmlParser = XMLParser(data: mutableData as Data)
+//        xmlParser.delegate = self
+//        xmlParser.parse()
+//        xmlParser.shouldResolveExternalEntities = true
+//    }
      // MARK: - XMLParserDelegate
     
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
